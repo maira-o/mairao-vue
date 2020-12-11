8aee6d300b6824c61e16865a03c0999db4592817
maira-o <mairasetecores@gmail.com>
2020-12-11 18:34:53 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <main id="app">
+    <img alt="erva emoji" src="./assets/logo.png"> 
+    <!-- <HelloWorld msg="brotando..."/> -->
+  </main>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10rem;
+  }
+
+  img {
+    animation: bounceIn 0.6s;
+  }
+
+  @keyframes bounceIn {
+  0% {
+    opacity: 1;
+    transform: scale(.3);
+  } 
+
+  50% {
+    opacity: 1;
+    transform: scale(1.05);
+  } 
+
+  70% {
+    opacity: 1;
+    transform: scale(.9);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+</style>
